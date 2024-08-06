1a9e1818f50d945cc694401858d1875a06ca7f20
pranay960 <166149093+pranay960@users.noreply.github.com>
2024-08-06 23:52:17 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,8 @@
+function changeWeight(operation) {
+    var weight = document.getElementById('weight');
+    var price = document.getElementById('price');
+    var weightChange = operation === '+' ? 1 : -1;
+    weight.value = parseInt(weight.value) + weightChange * 50;
+    var newPrice = parseInt(price.textContent.split(' ')[1]) + 2 * weightChange;
+    price.textContent = 'Price: ' + newPrice;
+}
